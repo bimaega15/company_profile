6c370b1b3ad792e6329cad31253303769d1f0e25
bimaega15 <bimaega15@gmail.com>
2023-10-14 08:06:32 +0700

    lebih clean code

--- Modules/Master/Resources/views/menu/renderTree.blade.php
+++ Modules/Master/Resources/views/menu/renderTree.blade.php
@@ -1,82 +1,16 @@
-<?php
+@php
+$structureTree = UtilsHelp::createStructureTree();
+$hiddenTree = UtilsHelp::handleSidebar($structureTree);
 
-$hiddenTree = UtilsHelp::handleSidebar($createTree);
-// Fungsi untuk merender data tree
-function renderTree($data, $parentId = null, $pushData = null)
-{
-    echo  '
-        <ol class="dd-list">';
-    foreach ($data as $index => $item) {
-        if (isset($pushData[$item['id']])) {
-            if ($pushData[$item['id']]) {
-                continue;
-            }
-        }
-
-        $menu_item = UtilsHelp::menuFilterById($item['id']);
-        $buttonUpdate = '
-            <a href="' . route('master.menu.edit', $menu_item->id) . '" class="btn btn-warning btn-edit btn-sm" style="padding: 0 5px;">
-                <i class="zmdi zmdi-edit" style="font-size: 10px;"></i>
-            </a>
-            ';
-        $buttonDelete = '
-            <button type="button" class="btn-delete btn btn-danger btn-sm" data-url="' . url('master/menu/' . $menu_item->id . '?_method=delete') . '" style="padding: 0 5px;">
-                <i class="zmdi zmdi-delete" style="font-size: 10px;"></i>
-            </button>
-            ';
-
-        if ($item['children'] === null && ($parentId === null || in_array($item['id'], $parentId))) {
-            echo  '
-            <li class="dd-item dd3-item" data-id="' . $item['id'] . '">
-                <div class="dd-handle dd3-handle"></div>
-                <div class="dd3-content p-0">
-                    <div class="d-flex justify-content-between align-items-center" style="padding-left: 50px; padding-right: 10px;">
-                        <div>
-                            <a href="' . url($menu_item->link_menu) . '">
-                            ' . $menu_item->icon_menu . ' &nbsp; ' . $menu_item->nama_menu . '
-                            </a>
-                        </div>
-                        <div>
-                            ' . $buttonUpdate . '
-                            ' . $buttonDelete . '
-                        </div>
-                    </div>
-                </div>
-            </li>
-            ';
-        } elseif ($item['children'] !== null && ($parentId === null || in_array($item['id'], $parentId))) {
-            echo  '
-                <li class="dd-item dd3-item" data-id="' . $item['id'] . '">
-                    <div class="dd-handle dd3-handle"></div>
-                        <div class="dd3-content p-0">
-                            <div class="d-flex justify-content-between align-items-center" style="padding-left: 15px; padding-right: 10px;">
-                            <div>
-                                <a href="' . url($menu_item->link_menu) . '">
-                                ' . $menu_item->icon_menu . ' &nbsp; ' . $menu_item->nama_menu . '
-                                </a>
-                            </div>
-                            <div>
-                                ' . $buttonUpdate . '
-                                ' . $buttonDelete . '
-                            </div>
-                        </div>
-                    </div>';
-            $childIds = $item['children'];
-            renderTree($data, $childIds);
-            echo '
-                </li>
-            ';
-        }
-    }
-    echo  '
-        </ol>';
-}
-?>
+ob_start();
+echo UtilsHelp::renderTree($structureTree, null, $hiddenTree);
+$outputTree = ob_get_clean();
+@endphp
 
 <div class="clearfix m-b-20">
     <div id="output_menu">
         <div class="dd nestable-with-handle">
-            {!! renderTree($createTree, null, $hiddenTree) !!}
+            {!! $outputTree !!}
         </div>
     </div>
 </div>
--- app/Http/Helpers/UtilsHelper.php
+++ app/Http/Helpers/UtilsHelper.php
@@ -150,4 +150,74 @@ class UtilsHelper
             }
         }
     }
+
+    public static function renderTree($data, $parentId = null, $pushData = null)
+    {
+        echo  '
+            <ol class="dd-list">';
+        foreach ($data as $index => $item) {
+            if (isset($pushData[$item['id']])) {
+                if ($pushData[$item['id']]) {
+                    continue;
+                }
+            }
+
+            $menu_item = UtilsHelper::menuFilterById($item['id']);
+            $buttonUpdate = '
+                <a href="' . route('master.menu.edit', $menu_item->id) . '" class="btn btn-warning btn-edit btn-sm" style="padding: 0 5px;">
+                    <i class="zmdi zmdi-edit" style="font-size: 10px;"></i>
+                </a>
+                ';
+            $buttonDelete = '
+                <button type="button" class="btn-delete btn btn-danger btn-sm" data-url="' . url('master/menu/' . $menu_item->id . '?_method=delete') . '" style="padding: 0 5px;">
+                    <i class="zmdi zmdi-delete" style="font-size: 10px;"></i>
+                </button>
+                ';
+
+            if ($item['children'] === null && ($parentId === null || in_array($item['id'], $parentId))) {
+                echo  '
+                <li class="dd-item dd3-item" data-id="' . $item['id'] . '">
+                    <div class="dd-handle dd3-handle"></div>
+                    <div class="dd3-content p-0">
+                        <div class="d-flex justify-content-between align-items-center" style="padding-left: 50px; padding-right: 10px;">
+                            <div>
+                                <a href="' . url($menu_item->link_menu) . '">
+                                ' . $menu_item->icon_menu . ' &nbsp; ' . $menu_item->nama_menu . '
+                                </a>
+                            </div>
+                            <div>
+                                ' . $buttonUpdate . '
+                                ' . $buttonDelete . '
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                ';
+            } elseif ($item['children'] !== null && ($parentId === null || in_array($item['id'], $parentId))) {
+                echo  '
+                    <li class="dd-item dd3-item" data-id="' . $item['id'] . '">
+                        <div class="dd-handle dd3-handle"></div>
+                            <div class="dd3-content p-0">
+                                <div class="d-flex justify-content-between align-items-center" style="padding-left: 15px; padding-right: 10px;">
+                                <div>
+                                    <a href="' . url($menu_item->link_menu) . '">
+                                    ' . $menu_item->icon_menu . ' &nbsp; ' . $menu_item->nama_menu . '
+                                    </a>
+                                </div>
+                                <div>
+                                    ' . $buttonUpdate . '
+                                    ' . $buttonDelete . '
+                                </div>
+                            </div>
+                        </div>';
+                $childIds = $item['children'];
+                UtilsHelper::renderTree($data, $childIds);
+                echo '
+                    </li>
+                ';
+            }
+        }
+        echo  '
+            </ol>';
+    }
 }
