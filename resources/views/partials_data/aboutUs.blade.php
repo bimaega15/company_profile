0b16192692d3bca1cadac2b9fbd8bb30e3dc65ff
syahrialrubbama <rubbamasyahrial17@gmail.com>
2023-10-31 09:46:28 +0700

    update

--- resources/views/partials_data/aboutUs.blade.php
+++ resources/views/partials_data/aboutUs.blade.php
@@ -3,9 +3,12 @@
         <div class="row align-items-center">
             <div class="col-lg-6">
                 <div class="about-img about-img-two">
-                    <img data-cfsrc="{{ asset('upload/settings/perusahaan/'.UtilsHelp::settingApp()->perusahaan_settings) }}" alt="{{ UtilsHelp::settingApp()->perusahaan_settings }}" style="display:none;visibility:hidden; width: 100%;"><noscript>
+                    <img data-cfsrc="{{ asset('upload/settings/perusahaan/' . UtilsHelp::settingApp()->perusahaan_settings) }}"
+                        alt="{{ UtilsHelp::settingApp()->perusahaan_settings }}"
+                        style="display:none;visibility:hidden; width: 100%;"><noscript>
 
-                        <img src="{{ asset('upload/settings/perusahaan/'.UtilsHelp::settingApp()->perusahaan_settings) }}" alt="{{ UtilsHelp::settingApp()->perusahaan_settings }}"></noscript>
+                        <img src="{{ asset('upload/settings/perusahaan/' . UtilsHelp::settingApp()->perusahaan_settings) }}"
+                            alt="{{ UtilsHelp::settingApp()->perusahaan_settings }}"></noscript>
 
                     <div class="experience">
                         <div class>
@@ -18,7 +21,7 @@
                 <div class="about-content">
                     <span class="top-title">Tentang Kami</span>
                     <h2>Visi & Misi</h2>
-                    <p>{!! (UtilsHelp::aboutUs()->visimisi_tentangkami) !!}</p>
+                    <p>{!! UtilsHelp::aboutUs()->visimisi_tentangkami !!}</p>
                 </div>
             </div>
         </div>
@@ -28,14 +31,16 @@
                 <div class="about-content">
                     <span class="top-title">Tentang Kami</span>
                     <h2>Nilai nilai pada perusahaan</h2>
-                    <p>{!! (UtilsHelp::aboutUs()->nilainilai_tentangkami) !!}</p>
+                    <p>{!! UtilsHelp::aboutUs()->nilainilai_tentangkami !!}</p>
                 </div>
             </div>
-
             <div class="col-lg-6">
                 <div class="about-img about-img-two">
-                    <img data-cfsrc="{{ asset('upload/settings/logo/'.UtilsHelp::settingApp()->logo_settings) }}" alt="{{ UtilsHelp::settingApp()->logo_settings }}" style="display:none;visibility:hidden; width: 100%;"><noscript>
-                        <img src="{{ asset('upload/settings/logo/'.UtilsHelp::settingApp()->logo_settings) }}" alt="{{ UtilsHelp::settingApp()->logo_settings }}"></noscript>
+                    <img data-cfsrc="{{ asset('upload/settings/logo/' . UtilsHelp::settingApp()->logo_settings) }}"
+                        alt="{{ UtilsHelp::settingApp()->logo_settings }}"
+                        style="display:none;visibility:hidden; width: 100%;"><noscript>
+                        <img src="{{ asset('upload/settings/logo/' . UtilsHelp::settingApp()->logo_settings) }}"
+                            alt="{{ UtilsHelp::settingApp()->logo_settings }}"></noscript>
                 </div>
             </div>
         </div>
