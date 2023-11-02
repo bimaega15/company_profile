d878d0bceb684b76acaaed6274a9d8610696d127
bimaega15 <bimaega15@gmail.com>
2023-11-02 15:44:45 +0700

    malah hilang views nya

--- config/mail.php
+++ config/mail.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Models\Setting;
-
-$getSetting = Setting::first();
 return [
 
     /*
@@ -39,16 +36,13 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => $getSetting->setting_acountemail,
-            'password' => $getSetting->setting_acountpassword,
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
-
-            'auth_mode'  => null,
-            'verify_peer' => false,
         ],
 
         'ses' => [
@@ -84,12 +78,6 @@ return [
                 'log',
             ],
         ],
-        'stream' => [
-            'ssl' => [
-                'allow_self_signed' => true,
-                'verify_peer' => false,
-            ],
-        ],
     ],
 
     /*
@@ -104,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', $getSetting->setting_acountemail),
-        'name' => env('MAIL_FROM_NAME', $getSetting->setting_namapengirim),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
---
+++ database/migrations/2023_11_02_153846_add_column_to_produk.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('produk', function (Blueprint $table) {
+            $table->string('icon_produk');
+            $table->string('deskripsisingkat_produk');
+            $table->string('kategori_produk');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('produk', function (Blueprint $table) {
+            $table->dropColumn('icon_produk');
+            $table->dropColumn('deskripsisingkat_produk');
+            $table->dropColumn('kategori_produk');
+        });
+    }
+};
